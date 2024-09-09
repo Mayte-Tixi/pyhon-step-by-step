07ef1df40209b2f56028742d6936897e5700b0c0
Mayte-Tixi <azoguezmayte@gmail.com>
2024-09-10 02:15:00 +0300

    agregue el practicas.py

---
+++ practicas.py
@@ -0,0 +1,27 @@
+#practicas de los tipos de datos
+#entero
+n=10
+n1=True
+n2=6.4
+mensaje="Si puedo soy excelente profesional y programadora amo aprender la IA"
+# tipos de datos
+print(type(n))
+print(type(n1))
+print(type(n2))
+print(type(mensaje))
+#transformar los tipos de datos
+print(str(n))
+print(int(n2))
+#funciones 
+#transformar a mayuscula
+mayuscula=mensaje.upper()
+minuscula=mensaje.lower()
+print(mayuscula)
+print(minuscula)
+transformar=mensaje.replace("profesional", " persona")
+print(transformar)
+#PRACTICAS DE ESTRUCTURAS DE DATOS(lISTAS, TUPLAS )
+#**LISTAS 
+lista=[3,5.5,4,'soy muy inteligente yo puedo',True]
+print('la posicion impresa de la lista es=',lista[3])
+#
