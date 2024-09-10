caaa1959ba3dff2d1422285d87c1828623d45213
Mayte-Tixi <azoguezmayte@gmail.com>
2024-09-10 10:31:13 -0500

    agregue al documento practicas.py la practica de estructura de datos Listas y sus funciones

--- practicas.py
+++ practicas.py
@@ -12,7 +12,7 @@ print(type(mensaje))
 #transformar los tipos de datos
 print(str(n))
 print(int(n2))
-#funciones 
+#funciones que se puede utilizar con el tipo de datos String 
 #transformar a mayuscula
 mayuscula=mensaje.upper()
 minuscula=mensaje.lower()
@@ -20,8 +20,41 @@ print(mayuscula)
 print(minuscula)
 transformar=mensaje.replace("profesional", " persona")
 print(transformar)
-#PRACTICAS DE ESTRUCTURAS DE DATOS(lISTAS, TUPLAS )
+#PRACTICAS DE ESTRUCTURAS DE DATOS(lISTAS, TUPLAS, DICCIONARIOS, CONJUNTOS )
 #**LISTAS 
+# en las listas pueden agregar todo tipo de datos 
 lista=[3,5.5,4,'soy muy inteligente yo puedo',True]
+# recuerden que la lista recorre desde la posicion 0 : hasta n datos dentro de la lista 
 print('la posicion impresa de la lista es=',lista[3])
-#
+print('imprimir desde hasta=',lista[0:3])
+print(len(lista))
+#la funcion iundex indica  la posicion que ocupa un objeto dentro de una lista
+print(lista.index('soy muy inteligente yo puedo'))
+# la funcion inset agrega datos a la lista seleccuionando la posicion al lado izquierdo
+#append ingresa valores a la lista al final, tambien se puede agregar sub listas dentro de la lista que tenemos creado.
+lista.insert(1,"soy muy inteligente")
+lista.append([2,1,3,"somos 2"])
+print(lista[6])
+#funcion extend agreda elementos dentro de la lista elemento x elemento
+lista.extend(['zorro','zorro','zorro','zorro','leon','jirafa'])
+
+#funcion para eliminar elementos de una lista
+lista.remove('leon')
+#funcion para contabilizar los elementos que se encuentran repetidos dentro de una lista
+print(lista)
+print('la palabra zorro dentro de la lista se repite=',lista.count('zorro'))
+#funcion que ordena los elementos de una lista del final hacia adelante
+lista1=[1,3,4,5,9,2,6]
+#lista de menor a meyor
+lista1.sort()
+#lista de mayor a menor
+lista1.sort(reverse=True)
+
+#funcion que eliminar un dato de la lista 
+lista1.pop(0)
+print(lista1)
+
+
+
+#* TUPLAS 
+
