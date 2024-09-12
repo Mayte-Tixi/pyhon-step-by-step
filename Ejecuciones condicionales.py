aac75feab3b877ae0533bfa5c7a779a5677d2721
Mayte-Tixi <azoguezmayte@gmail.com>
2024-09-12 17:32:44 -0500

    agregue modificaciones al documento Ejecuciones condicionales.py

---
+++ Ejecuciones condicionales.py
@@ -0,0 +1,17 @@
+#* IF
+#Nos permite ejecutar un bloque de instrucciones y evaluar el valor boleano que nos devuelve (True or False)
+edad=int(input())
+
+if (edad >18 and edad <50):
+    if (edad>18 and edad<25):
+        print(' hola menor')
+    else:
+        print('hola mayores')
+elif(edad>40 and edad>75):
+    if(edad>40 and edad<25):
+        print('hola adulto')
+    else:
+     print('hola muy mayores')              
+else:
+    print('la edad ingresada es esta fuera del rango')
+
